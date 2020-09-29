bd5ce2ebd163b466ac660a5edd9795606a86f709
Tomasz Tarkowski <tarkowskixtomasz@gmail.com>
2020-09-29 23:30:00 +0200

    added fetch with axios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
+import Movie from './components/Movie';
+
+
+
+const API_KEY = 'ee8b648ac04179ef14616f3b26f6499e';
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?sort_by=popilarity.desc&api_key=${API_KEY}`
+const IMG_API = 'https://image.tmdb.org/t/p/w1280';
+const SEARCH_API = 'https://api.themoviedb.org/3/find/{external_id}?api_key=<<api_key>>&language=en-US&external_source=imdb_id';
+
+
 function App() {
+
+    const [movies, setMovies] = useState([]);
+    
+
+    
+
+  useEffect(()=>{
+
+    async function fetchData(){
+      const request = await axios.get(FEATURED_API)
+      console.log(request.data.results);
+      setMovies(request.data.results);
+      return request
+    }
+     
+    fetchData();
+    
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      {console.log(movies)}
+      {movies.length > 0  && 
+      movies.map((movie)=> <Movie key={movie.id} data={movie} />)}
+   </div>
+  )
 }
 
 export default App;
---
+++ src/components/Movie.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Movie = () => {
+
+    return (
+        <div className='movie'>
+            This is a movie component
+        </div>
+
+    )
+
+    
+
+} 
+
+
+export default Movie;
