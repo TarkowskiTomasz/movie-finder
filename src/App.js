1f9c23353915423270d1c1a0d1eb9a483f7feca5
Tomasz Tarkowski <tarkowskixtomasz@gmail.com>
2020-09-30 14:15:14 +0200

    added header, before styled

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Movie from './components/Movie';
 
 
 const API_KEY = 'ee8b648ac04179ef14616f3b26f6499e';
-const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?sort_by=popilarity.desc&api_key=${API_KEY}`
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?sort_by=popilarity.asc&api_key=${API_KEY}`
 const IMG_API = 'https://image.tmdb.org/t/p/w1280';
 const SEARCH_API = 'https://api.themoviedb.org/3/find/{external_id}?api_key=<<api_key>>&language=en-US&external_source=imdb_id';
 
@@ -34,10 +34,13 @@ function App() {
 
 
   return (
-    <div className="App">
+    <div className="movie-container">
+      <header >
+                <input className="search-iput" type="text" placeholder="Search" />
+            </header>
       {console.log(movies)}
       {movies.length > 0  && 
-      movies.map((movie)=> <Movie key={movie.id} data={movie} />)}
+      movies.map((movie)=> <Movie key={movie.id} {...movie} />)}
    </div>
   )
 }
