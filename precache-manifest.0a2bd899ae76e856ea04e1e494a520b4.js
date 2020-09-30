5309d15e330e247f12e5cde589d570f32788d535
Tomasz Tarkowski <tarkowskixtomasz@gmail.com>
2020-09-30 21:53:12 +0200

    Updates

---
+++ precache-manifest.0a2bd899ae76e856ea04e1e494a520b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b657bbfb6fe3b8ec8ccaf0c887c37949",
+    "url": "/nazwa/index.html"
+  },
+  {
+    "revision": "cad4abc90570de5619ed",
+    "url": "/nazwa/static/css/main.eecd3c9a.chunk.css"
+  },
+  {
+    "revision": "4400b734f68d149ef7c3",
+    "url": "/nazwa/static/js/2.d0377ef9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nazwa/static/js/2.d0377ef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cad4abc90570de5619ed",
+    "url": "/nazwa/static/js/main.2a3a6533.chunk.js"
+  },
+  {
+    "revision": "33ffcdebf45f04bda6f5",
+    "url": "/nazwa/static/js/runtime-main.91631435.js"
+  }
+]);
